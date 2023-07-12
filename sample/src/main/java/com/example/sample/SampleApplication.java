c50d3c6034d84e8ddc0994ce7f4ee7ff409642a9
J-Jaeh <1wo2gur@naver.com>
2023-07-12 23:52:39 +0900

    [refac] 입력값 클래스로 분리

--- sample/src/main/java/com/example/sample/SampleApplication.java
+++ sample/src/main/java/com/example/sample/SampleApplication.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class SampleApplication {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter two number and an operator ( e.g 1 + 2): ");
-        String result = scanner.nextLine();
-        String[] parts = result.split(" ");
+
         long num1 = Long.parseLong(parts[0]);
         long num2 = Long.parseLong(parts[2]);
         String operator = parts[1];
+
+        long answer = new Calculator().calculate(num1, operator, num2);
+        System.out.println(answer);
 
     }
 
