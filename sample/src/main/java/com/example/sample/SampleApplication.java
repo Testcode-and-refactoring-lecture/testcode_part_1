8453fe3afb8d48fe9a01d5a7a6bde5e0fa089a0c
J-Jaeh <1wo2gur@naver.com>
2023-07-13 00:23:10 +0900

    [refac] 입력받는 일을 분리

--- sample/src/main/java/com/example/sample/SampleApplication.java
+++ sample/src/main/java/com/example/sample/SampleApplication.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SampleApplication {
     public static void main(String[] args) {
 
+        String[] parts = new CalculationRequestReader().read();
         long num1 = Long.parseLong(parts[0]);
         long num2 = Long.parseLong(parts[2]);
         String operator = parts[1];
